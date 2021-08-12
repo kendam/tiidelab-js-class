07ce344828963a6754eac3a133418ddea48f6052
unknown <farukadekunle@gmail.com>
2021-08-12 11:43:50 +0100

    My ussd assignment

---
+++ ussd.js
@@ -0,0 +1,56 @@
+let welcome, page1, page2, 
+page3, page4, page5, page6, 
+page7, page8, page9, page10, page11;
+
+welcome = Number(welcome);
+page2 = Number(page2);
+page3 = Number(page3);
+page4 = Number(page4);
+page5 = Number(page5);
+page6 = Number(page6);
+page7 = Number(page7);
+page8 = Number(page8);
+
+welcome = prompt('Welcome\n' + '1- Airtime-self\n' + '2- Airtime-others\n' + '3- Data\n' + '4- Transfer\n' + '5- Cancel\n');
+page2 = prompt('Enter Amount');
+page12 = alert('Thank You');
+page3 = prompt('Enter Number');
+
+if(welcome === 1){
+    prompt(page2)
+}
+else if(welcome === 2){
+    prompt(page3)
+}
+else if(welcome === 3){
+    prompt(page5)
+}
+else if(welcome === 4){
+    prompt(page6)
+}
+else if(welcome === 5){
+    prompt(page12)
+}else{
+    prompt(page12)
+}
+
+
+
+
+
+// page5 = prompt('Data Plan\n' + '1- 1GB FOR #500\n' + '2- 2GB FOR #1000\n' + '3- 3GB FOR #1200\n');
+// page6 = prompt('Enter Account Number');
+// page11 = confirm('Click OK to continue transaction');
+// page10 = alert('Transaction Failed');
+
+
+
+
+
+
+
+
+// page4 = prompt('Select Network\n' + '1- MTN\n' + '2- AIRTEL\n' + '3- GLO\n' + '4- 9MOBILE\n');
+// page7 = prompt('SELECT BANK\n' + '1- Firstbank\n' + '1- Gtbank\n' + '1- Accessbank\n' + '1- UBA\n' + '1- Jaiz Bank\n');
+// page8 = prompt('Enter PIN to confirm payment');
+// page9 = alert('Transaction Successful');
